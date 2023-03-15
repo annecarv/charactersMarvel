2c710065692e7ae8835f839c997e15dc2380f3fb
annetherezaprofissional@gmai.com <annetherezaprofissional@gmail.com>
2023-03-15 16:01:18 -0300

    fix: style writing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-from hashConfig import hashConfig
+from hashConfig import hash_config
 import dotenv
 from dotenv import load_dotenv
 import os
@@ -8,27 +8,27 @@ import datetime
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-timeStamp = datetime.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
+time_stamp = datetime.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
 
-privateKey = os.getenv('privateKey')
-publicKey = os.getenv('publicKey')
-baseEndpoint = os.getenv('baseEndpoint')
+private_key = os.getenv('privateKey')
+public_key = os.getenv('publicKey')
+base_endpoint = os.getenv('baseEndpoint')
 
 params = {
-    'ts': timeStamp,
-    'apikey': publicKey,
-    'hash': hashConfig(),
+    'ts': time_stamp,
+    'apikey': public_key,
+    'hash': hash_config(),
     'offset': 0,
     'limit': 100
 }
 
-def getCharacters():
+def get_characters():
 
-    responseRequest = requests.get(baseEndpoint, params=params).json()
+    response_request = requests.get(base_endpoint, params=params).json()
 
-    dataCollection = []
+    data_collection = []
 
-    for item in responseRequest['data']['results']:
+    for item in response_request['data']['results']:
         id = item["id"]
         name = item["name"]
         description = item["description"]
@@ -37,20 +37,20 @@ def getCharacters():
         stories = item["stories"]["available"]
         events = item["events"]["available"]
 
-        dataSet = {"id": id, "name": name, "description": description, "comics": comics, "series": series, "stories": stories, "events" : events }
+        data_set = {"id": id, "name": name, "description": description, "comics": comics, "series": series, "stories": stories, "events" : events }
 
-        dataCollection.append(dataSet)
+        data_collection.append(data_set)
 
-        df = pd.DataFrame(dataCollection, dtype=None, columns = ['id','name','description','comics','series','stories','events'])
+        df = pd.DataFrame(data_collection, dtype=None, columns = ['id','name','description','comics','series','stories','events'])
         df.to_csv('marvelTricks.csv', index = False)
 
-        htmlView = df.to_html(justify='center')
-        htmlConfig = open("index.html", "w")
-        htmlConfig.write(htmlView)
-        htmlConfig.close()
+        html_view = df.to_html(justify='center')
+        html_config = open("index.html", "w")
+        html_config.write(html_view)
+        html_config.close()
 
     print(df)
-    print(htmlView)
+    print(html_view)
 
 
-getCharacters()
+get_characters()
